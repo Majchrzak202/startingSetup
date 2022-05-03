7e7513a4f9d9e44ae30e7eadc8b2ccefc9abe409
MarcinMaster <m.luba505@gmail.com>
2022-05-03 08:06:56 +0200

    first comitment

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+import { motion } from 'framer-motion/dist/framer-motion'
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && (
+        <motion.button 
+        onClick={startEditingHandler}
+        initial={{scale: 0.8}}
+        animation={{scale: 1}}
+        >Add New Expense</motion.button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
